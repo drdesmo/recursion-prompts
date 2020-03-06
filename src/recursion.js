65d510607ef4cf254419e1a266088b42771e9b0e
Adam Smith <adam@mediascope.com>
2020-03-05 18:18:58 -0700

    Problems 1-6 completed and passed tests.

--- src/recursion.js
+++ src/recursion.js
@@ -7,31 +7,130 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+    
+    if (n < 0){
+        return null;
+    }
+
+    if (n === 0){
+        return 1;
+    }
+    
+    return (n * factorial(n-1));
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+
+    var newArray = Array.from(array);
+
+    if (array.length === 0){
+        return 0;
+    }
+    if (newArray.length === 1){
+        return newArray[0];
+    }
+
+    else {
+    
+        return newArray.pop() + sum(newArray);
+        
+    }
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
-};
+
+    // Condition checking
+
+    if (array.length === 0){
+        return 0;
+    }
+
+    // Create variables
+
+    var sum = 0;
+
+    // Iterate over the array
+
+    for (var i = 0; i < array.length; i++){
+        var current = array[i]; // Current index in array
+        if (Array.isArray(current)){
+            sum += arraySum(current); // We must go deeper!
+        }
+        else{
+            sum += current;
+        }
+        
+    }
+    return sum;
+    // If it is an array call this function on the current index
+    // If it isn't an array, add the value to sum
+    // Return
+
+}
+
+
 
 // 4. Check if a number is even.
+// Game plan:  We're going to subtract two from the number by calling this function repeatedly until we get zero
+// ... or negative one.
+// The base case will be if we hit 0.  If so, return true.  If we hit <= 1, it's not going to be even.
+
 var isEven = function(n) {
+
+    n = Math.abs(n);
+    
+    if (n === 0){
+        return true;
+    }
+
+    n -= 2; // Odd check decrement
+
+    if (n < 0){
+        return false;
+    }
+
+    return (isEven(n));
 };
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
+
 var sumBelow = function(n) {
+    
+        if(n===1 || n=== 0){
+            return 0;
+        }
+        // If a negative number, other directino.
+
+        else if(n<0){
+          return (n+1) + sumBelow (n+1);
+        }
+
+        return (n-1) + sumBelow(n-1);
+    
 };
 
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+
+    // Base case, let's end it here if we can.
+    if(n===1 || n=== 0){
+        return 0;
+    }
+    // If a negative number, other direction.
+
+    else if(n<0){
+      return (n+1) + sumBelow (n+1);
+    }
+
+    // Return if we are still on this path.
+    return (n-1) + sumBelow(n-1);
 };
 
 // 7. Compute the exponent of a number.
@@ -51,6 +150,7 @@ var powerOfTwo = function(n) {
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+
 };
 
 // 10. Write a function that determines if a string is a palindrome.
